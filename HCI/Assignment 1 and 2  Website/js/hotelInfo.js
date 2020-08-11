a38485b4bb1d66e7fa5b67823ca2f0d90dcb01a8
Yatish Kelkar <yatish1606@gmail.com>
2020-08-12 00:05:52 +0530

    Complete styling

--- HCI/Assignment 1 and 2  Website/js/hotelInfo.js
+++ HCI/Assignment 1 and 2  Website/js/hotelInfo.js
@@ -3,8 +3,8 @@ const b = document.querySelector('.book')
 
 window.onload = function runOnLoad() {
 
-    const a = JSON.parse(localStorage.getItem('hotelData'))
-    console.log(a)
-    a.prices.forEach(i => b.innerHTML += `<h1>${i.name}</h1>`)
+    // const a = JSON.parse(localStorage.getItem('hotelData'))
+    // console.log(a)
+    // a.prices.forEach(i => b.innerHTML += `<h1>${i.name}</h1>`)
     console.log(JSON.parse(localStorage.getItem('hotelData')))
 }
