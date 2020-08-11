905f97b4dfac979dd99c73da17cde79131a3c79c
Yatish Kelkar <yatish1606@gmail.com>
2020-08-11 16:24:58 +0530

    hotelInfo.html

--- HCI/Assignment 1 and 2  Website/js/hotelInfo.js
+++ HCI/Assignment 1 and 2  Website/js/hotelInfo.js
@@ -1,3 +1,10 @@
+const b = document.querySelector('.book')
+
+
 window.onload = function runOnLoad() {
+
+    const a = JSON.parse(localStorage.getItem('hotelData'))
+    console.log(a)
+    a.prices.forEach(i => b.innerHTML += `<h1>${i.name}</h1>`)
     console.log(JSON.parse(localStorage.getItem('hotelData')))
 }
