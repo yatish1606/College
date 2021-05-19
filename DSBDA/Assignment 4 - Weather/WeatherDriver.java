3c9992993b6bb6640f7c79fbdf83c064c909f542
Yatish Kelkar <yatish1606@gmail.com>
2021-05-19 12:00:53 +0530

    Update

---
+++ DSBDA/Assignment 4 - Weather/WeatherDriver.java
@@ -0,0 +1,63 @@
+package Weather;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WeatherDriver {
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration con = new Configuration();
+        Job job = new Job(con, "Weather");
+
+        String string;
+
+        job.setJarByClass(Weather.WeatherDriver.class);
+        job.setMapperClass(Weather.WeatherMapper.class);
+        job.setReducerClass(Weather.WeatherReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[1]));
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        job.waitForCompletion(true);
+
+        FileSystem fs = FileSystem.get(con);
+        FileStatus[] status = fs.listStatus(new Path("hdfs://localhost:9000"+args[2]));
+        FSDataInputStream fd = fs.open(status[1].getPath());
+
+        string = fd.readLine();
+
+        float max = -9999, min = 9999, temp;
+        String minYear = null, maxYear = null;
+
+        while(string != null) {
+            String [] tokens = string.split("\t");
+            temp = Integer.parseInt(tokens[1]);
+
+            if(temp > max) {
+                max = temp;
+                maxYear = tokens[0];
+                continue;
+            }
+            if(temp < min) {
+                min = temp;
+                minYear = tokens[0];
+            }
+            string = fd.readLine();
+        }
+
+        System.out.println("Maximum temperature : " + max/10 + " in the year " + maxYear);
+        System.out.println("Minimum temperature : " + min/10 + " in the year " + minYear);
+    }
+}
---
+++ DSBDA/Assignment 4 - Weather/WeatherReducer.java
@@ -0,0 +1,31 @@
+package Weather;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class WeatherReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+        int max = -9999;
+        int min = 9999;
+
+        for(IntWritable value : values) {
+            if(value.get() < min)
+                min = value.get();
+            if(value.get() > max)
+                max = value.get();
+        }
+
+//        for(IntWritable value : values) {
+//            min = Math.min(min, value.get());
+//        }
+
+        context.write(key, new IntWritable(max));
+        context.write(key, new IntWritable(min));
+    }
+
+}
